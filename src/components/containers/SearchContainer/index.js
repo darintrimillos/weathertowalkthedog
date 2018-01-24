cea24bf4cc37615c983350b45f4471c95a284a29
Darin Trimillos <darin@trimillos.com>
2018-01-24 15:59:06 -0800

    Can get geolocation

--- src/components/containers/SearchContainer/index.js
+++ src/components/containers/SearchContainer/index.js
@@ -5,12 +5,27 @@ class SearchContainer extends Component {
     constructor(props) {
         super(props);
 
+        this.getLocation = this.getLocation.bind(this);
+        this.showLocationData = this.showLocationData.bind(this);
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showLocationData);
+        } else {
+            alert('cannot get location');
+        }
+        return;
+    }
+
+    showLocationData(pos) {
+        console.log(pos);
     }
 
     render() {
         return (
             <section className="search-container">
-                <button>Get Weather</button>
+                <button onClick={this.getLocation}>Get Weather</button>
             </section>
         );
     }

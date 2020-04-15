25e7e94db94d218195783465f2664efe29e0edda
Darin Trimillos <darin@trimillos.com>
2020-04-15 15:28:47 -0700

    update font implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-logo">Weather to Walk the Dog 24</h1>
+        <p>This is some regular text.</p>
+        <p>40 degrees.</p>
       </header>
     </div>
   );

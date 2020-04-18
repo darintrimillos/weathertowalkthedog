bbd373835091b5a763de4e5f323f9bc50576069c
Darin Trimillos <darin@trimillos.com>
2020-04-18 15:16:06 -0700

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Container from './components/container/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-logo">Weather to Walk the Dog 24</h1>
+        <Container />
         <p>This is some regular text.</p>
         <p>40 degrees.</p>
       </header>
---
+++ src/components/container/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// interface State {
+//     weatherData: object;
+// };
+
+class Container extends React.Component {
+    state: {};
+
+    render() {
+        return <h1>Hello, darin</h1>;
+    }
+}
+
+export default Container;

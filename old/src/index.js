6916050ae607a5ebde82227970abc2c50ec91a5e
Darin Trimillos <darin@trimillos.com>
2020-04-21 15:28:12 -0700

    Delete old files

--- old/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppContainer from './components/containers/AppContainer';
-
-ReactDOM.render(
-  <AppContainer />,
-  document.getElementById('root')
-);

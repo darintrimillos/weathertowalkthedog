f7c9cdbee407fb85125f9d5e31f1096b4a34c621
Darin <darin@trimillos.com>
2020-05-05 12:08:11 -0700

    add get weather button

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-logo">Weather to Walk the Dog 24</h1>
-        <Container />
-        <p>This is some regular text.</p>
-        <p>40 degrees.</p>
+        <h1 className="App-logo">Weather to Walk the Dog</h1>
       </header>
+      <div>
+        <Container />
+      </div>
     </div>
   );
 }
--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
 export default class Container extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
+  constructor(props: object) {
+    super(props);
+    
+    this.getWeather = this.getWeather.bind(this);
+    this.getLocation = this.getLocation.bind(this);
+    this.handleLocationError = this.handleLocationError.bind(this);
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(this.getWeather, this.handleLocationError)
+    }
+  }
+
+  getWeather(locationData: any) {
+    console.log('getWeather');
+    console.log('geolocationPosition', locationData.latitude);
+  }
+
+  handleLocationError(error: any) {
+    console.log('error', error);
+  }
 
   render() {
     return (
       <div className="container">
-        <div className="header">
-          <h1>Weather to Walk the Dog!</h1>
-        </div>
+        <button onClick={this.getLocation}>
+          Get Weather
+        </button>
+        <h2>Right Now</h2>
+        <p>Current weather data</p>
+        <h2>Forecast</h2>
+        <p>Forecast data</p>
       </div>
     );
   }

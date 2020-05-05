25e903a777b3a84bd19f781af7e6e32bc56d4e45
Darin <darin@trimillos.com>
2020-05-05 15:49:10 -0700

    serve dev data

--- proxy/server.js
+++ proxy/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+
+const dummy = fs.readFileSync(path.resolve('./proxy/example.json'));
+
+if (process.env.NODE_ENV === 'dev') {
+  console.log('dev environment!');
+}
 
 app.get("/api", (req, res) => {
   res.send("Send an API response");
 });
 
 app.listen(8080, () => {
-  console.log('Example app listening on port 8080');  
+  console.log('Example app listening on port 8080', process.env.NODE_ENV);  
 });
